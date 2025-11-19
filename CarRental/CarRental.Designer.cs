565d0273a738b15173090942f5bf7f8f0f5df2b3
micahspencer-png <micahspencer@isu.edu>
2025-11-19 08:06:10 -0700

    added groupboxes

--- CarRental/CarRental.Designer.cs
+++ CarRental/CarRental.Designer.cs
@@ -28,18 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            groupBox3 = new GroupBox();
+            groupBox4 = new GroupBox();
+            groupBox5 = new GroupBox();
             SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(317, 264);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Location = new Point(12, 285);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(404, 122);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "groupBox2";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Location = new Point(429, 279);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(408, 132);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "groupBox3";
+            // 
+            // groupBox4
+            // 
+            groupBox4.Location = new Point(357, 16);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(495, 262);
+            groupBox4.TabIndex = 3;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "groupBox4";
+            // 
+            // groupBox5
+            // 
+            groupBox5.Location = new Point(24, 417);
+            groupBox5.Name = "groupBox5";
+            groupBox5.Size = new Size(802, 91);
+            groupBox5.TabIndex = 4;
+            groupBox5.TabStop = false;
+            groupBox5.Text = "groupBox5";
             // 
             // CarRental
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(874, 522);
+            Controls.Add(groupBox5);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
             Name = "CarRental";
             Text = "Car Rental Form";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
+        private GroupBox groupBox4;
+        private GroupBox groupBox5;
     }
 }
