df3e11b5d1ed0290bab349161ce73c63c8c1cbdc
micahspencer-png <micahspencer@isu.edu>
2025-11-23 12:22:03 -0700

    Set up front panels and all controls

--- CarRental/CarRental.Designer.cs
+++ CarRental/CarRental.Designer.cs
@@ -33,65 +33,281 @@
             groupBox3 = new GroupBox();
             groupBox4 = new GroupBox();
             groupBox5 = new GroupBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            textBox11 = new TextBox();
+            textBox12 = new TextBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox4.SuspendLayout();
+            groupBox5.SuspendLayout();
             SuspendLayout();
             // 
             // groupBox1
             // 
+            groupBox1.Controls.Add(textBox12);
+            groupBox1.Controls.Add(textBox8);
+            groupBox1.Controls.Add(textBox11);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(textBox10);
+            groupBox1.Controls.Add(textBox9);
+            groupBox1.Controls.Add(textBox6);
+            groupBox1.Controls.Add(textBox7);
             groupBox1.Location = new Point(12, 12);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(317, 264);
+            groupBox1.Size = new Size(317, 334);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
-            groupBox1.Text = "groupBox1";
+            groupBox1.Text = "Client Information";
             // 
             // groupBox2
             // 
-            groupBox2.Location = new Point(12, 285);
+            groupBox2.Controls.Add(radioButton2);
+            groupBox2.Controls.Add(radioButton1);
+            groupBox2.Location = new Point(12, 352);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(404, 122);
+            groupBox2.Size = new Size(404, 104);
             groupBox2.TabIndex = 1;
             groupBox2.TabStop = false;
-            groupBox2.Text = "groupBox2";
+            groupBox2.Text = "Is Odometer in Km or Mi?";
             // 
             // groupBox3
             // 
-            groupBox3.Location = new Point(429, 279);
+            groupBox3.Controls.Add(checkBox2);
+            groupBox3.Controls.Add(checkBox1);
+            groupBox3.Location = new Point(422, 299);
             groupBox3.Name = "groupBox3";
-            groupBox3.Size = new Size(408, 132);
+            groupBox3.Size = new Size(408, 105);
             groupBox3.TabIndex = 2;
             groupBox3.TabStop = false;
-            groupBox3.Text = "groupBox3";
+            groupBox3.Text = "Discounts";
             // 
             // groupBox4
             // 
+            groupBox4.Controls.Add(textBox4);
+            groupBox4.Controls.Add(textBox3);
+            groupBox4.Controls.Add(textBox2);
+            groupBox4.Controls.Add(textBox1);
             groupBox4.Location = new Point(357, 16);
             groupBox4.Name = "groupBox4";
             groupBox4.Size = new Size(495, 262);
             groupBox4.TabIndex = 3;
             groupBox4.TabStop = false;
-            groupBox4.Text = "groupBox4";
+            groupBox4.Text = "Cost Summary";
             // 
             // groupBox5
             // 
-            groupBox5.Location = new Point(24, 417);
+            groupBox5.Controls.Add(button3);
+            groupBox5.Controls.Add(button4);
+            groupBox5.Controls.Add(button2);
+            groupBox5.Controls.Add(button1);
+            groupBox5.Location = new Point(24, 452);
             groupBox5.Name = "groupBox5";
-            groupBox5.Size = new Size(802, 91);
+            groupBox5.Size = new Size(802, 76);
             groupBox5.TabIndex = 4;
             groupBox5.TabStop = false;
-            groupBox5.Text = "groupBox5";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(141, 26);
+            button1.Name = "button1";
+            button1.Size = new Size(116, 35);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(385, 26);
+            button2.Name = "button2";
+            button2.Size = new Size(116, 35);
+            button2.TabIndex = 0;
+            button2.Text = "button1";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(263, 26);
+            button3.Name = "button3";
+            button3.Size = new Size(116, 35);
+            button3.TabIndex = 0;
+            button3.Text = "button1";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(507, 26);
+            button4.Name = "button4";
+            button4.Size = new Size(116, 35);
+            button4.TabIndex = 0;
+            button4.Text = "button1";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(6, 37);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(117, 24);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "radioButton1";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(6, 67);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(117, 24);
+            radioButton2.TabIndex = 0;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "radioButton1";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(20, 43);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(101, 24);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(20, 73);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(101, 24);
+            checkBox2.TabIndex = 0;
+            checkBox2.Text = "checkBox1";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(272, 43);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(180, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(272, 76);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(180, 27);
+            textBox2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(272, 115);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(180, 27);
+            textBox3.TabIndex = 0;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(272, 148);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(180, 27);
+            textBox4.TabIndex = 0;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(175, 26);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(136, 27);
+            textBox5.TabIndex = 0;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(175, 59);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(136, 27);
+            textBox6.TabIndex = 0;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(175, 98);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(136, 27);
+            textBox7.TabIndex = 0;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(175, 131);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(136, 27);
+            textBox8.TabIndex = 0;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(175, 235);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(136, 27);
+            textBox9.TabIndex = 0;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(175, 196);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(136, 27);
+            textBox10.TabIndex = 0;
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(175, 163);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(136, 27);
+            textBox11.TabIndex = 0;
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(175, 268);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(136, 27);
+            textBox12.TabIndex = 0;
             // 
             // CarRental
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(874, 522);
+            ClientSize = new Size(874, 540);
             Controls.Add(groupBox5);
             Controls.Add(groupBox4);
             Controls.Add(groupBox3);
             Controls.Add(groupBox2);
             Controls.Add(groupBox1);
             Name = "CarRental";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Car Rental Form";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
+            groupBox5.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -102,5 +318,25 @@
         private GroupBox groupBox3;
         private GroupBox groupBox4;
         private GroupBox groupBox5;
+        private RadioButton radioButton1;
+        private Button button3;
+        private Button button4;
+        private Button button2;
+        private Button button1;
+        private TextBox textBox12;
+        private TextBox textBox8;
+        private TextBox textBox11;
+        private TextBox textBox5;
+        private TextBox textBox10;
+        private TextBox textBox9;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private RadioButton radioButton2;
+        private CheckBox checkBox2;
+        private CheckBox checkBox1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
     }
 }
--- CarRental/CarRental.cs
+++ CarRental/CarRental.cs
@@ -1,10 +1,21 @@
 namespace CarRental
 {
+    //Micah Spencer
+    //RCET2265
+    //Fall 2025
+    //Car Rental Form Program
+    //https://github.com/micahspencer-png/CarRental.git
     public partial class CarRental : Form
     {
         public CarRental()
         {
             InitializeComponent();
+            SetDefaults();
+        }
+
+        void SetDefaults() 
+        { 
+            
         }
     }
 }
